491736d288d866d757aad41ae09dcbaa23c0bc36
szymirak <144483539+szymirak@users.noreply.github.com>
2023-09-10 22:33:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('.sticky-header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    }
+});
