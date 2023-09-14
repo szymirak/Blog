1608e5f6fc6ae89fc38c7df4d526111e01aecf81
Szymon <144483539+szymirak@users.noreply.github.com>
2023-09-14 21:09:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-const header = document.querySelector('.sticky-header');
+// script.js
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) {
-        header.classList.add('sticky');
-    } else {
-        header.classList.remove('sticky');
-    }
+// Funkcja do wczytywania zawartości podstron
+function loadContent(path) {
+  fetch(path)
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("content").innerHTML = html;
+    });
+}
+
+// Obsługa nawigacji
+const navigationLinks = document.querySelectorAll("nav a");
+
+navigationLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    history.pushState({}, "", href);
+    loadContent(href + ".html");
+  });
 });
+
+// Obsługa nawigacji w przypadku załadowania strony
+window.addEventListener("popstate", () => {
+  loadContent(location.pathname + ".html");
+});
+
+// Wczytaj domyślną zawartość
+loadContent("omnie.html");
